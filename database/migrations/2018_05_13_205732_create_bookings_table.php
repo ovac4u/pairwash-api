6cf0c1a7ee9323570f31956a922bd736fa158274
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-05-13 21:41:37 +0000

    📒 Implement Booking tables

---
+++ database/migrations/2018_05_13_205732_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('vehicle_id');
+            $table->integer('user_id');
+            $table->string('location')->nullable();
+            $table->string('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
