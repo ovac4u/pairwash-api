df2eb20fc581d43f97e72f2684096e93bb1ce1d0
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-05-13 20:03:33 +0000

    Prepare authentication 👩‍💻

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -15,9 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('address');
+            $table->string('type')->enum(['vendor', 'client'])->default('client');
+            $table->string('password');
+
+            $table->timestamp('dob');
+
             $table->rememberToken();
             $table->timestamps();
         });
